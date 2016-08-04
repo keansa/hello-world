0f9a1ac07b15082c88b4ee3e8015ffcc74cc16d8
keansa <keansajenkins@gmail.com>
2016-08-04 14:39:42 +0530

    Initial upload

---
+++ myclass.java
@@ -0,0 +1,5 @@
+class myclass {
+public static void main(String arg[]){
+System.out.println("new class created");
+}
+}
---
+++ myclass2.java
@@ -0,0 +1,5 @@
+class myclass2 {
+public static void main(String arg[]){
+System.out.println("new class2 created");
+}
+}
